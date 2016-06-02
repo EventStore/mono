4493dfd1b87d809ce7a3dbd5a23bbac97b97df7f
Alex Rønne Petersen <alexrp@xamarin.com>
2016-06-02 11:27:38 +0200

    [bcl] Avoid ObjectDisposedException in EndPointListener.
    This fixes a regression introduced in 123aaa5db07fe464d12ea178ecc3f27990cc1a72.
    We would sometimes see an ObjectDisposedException when calling AcceptAsync ()
    because that patch removed the try-catch around it.

--- mcs/class/System/System.Net/EndPointListener.cs
+++ mcs/class/System/System.Net/EndPointListener.cs
@@ -71,7 +71,8 @@ namespace System.Net {
 			SocketAsyncEventArgs args = new SocketAsyncEventArgs ();
 			args.UserToken = this;
 			args.Completed += OnAccept;
-			Accept (sock, args);
+			Socket dummy = null;
+			Accept (sock, args, ref dummy);
 			prefixes = new Hashtable ();
 			unregistered = new Dictionary<HttpConnection, HttpConnection> ();
 		}
@@ -96,9 +97,21 @@ namespace System.Net {
 			}
 		}
 
-		static void Accept (Socket socket, SocketAsyncEventArgs e) {
+		static void Accept (Socket socket, SocketAsyncEventArgs e, ref Socket accepted) {
 			e.AcceptSocket = null;
-			var asyn = socket.AcceptAsync(e);
+			bool asyn;
+			try {
+				asyn = socket.AcceptAsync(e);
+			} catch {
+				if (accepted != null) {
+					try {
+						accepted.Close ();
+					} catch {
+					}
+					accepted = null;
+				}
+				return;
+			}
 			if (!asyn) {
 				ProcessAccept(e);
 			}
@@ -114,7 +127,7 @@ namespace System.Net {
 			EndPointListener epl = (EndPointListener) args.UserToken;
 
 
-			Accept (epl.sock, args);
+			Accept (epl.sock, args, ref accepted);
 			if (accepted == null)
 				return;
 
